2290f8efb94b853958019d0ec013caa3922273da
Max Base <MaxBaseCode@Gmail.Com>
2018-06-26 18:18:23 +0430

    Update JsonParser.php

--- JsonParser.php
+++ JsonParser.php
@@ -2,15 +2,31 @@
 abstract class JsonStatus
 {
 	const Normal = 0;
-	const Key = 1;
+	const String = 1;
 	const Value = 2;
+}
+abstract class JsonPosition
+{
+	const Key=0;
+	const Value=1;
+}
+abstract class JsonType
+{
+	const Null=0;
+	const Integer=1;
+	const Fraction=2;
+	const Exponent=3;
+	const String=4;
+	const Boolean=5;
+	const Array=6;
+	const Object=7;
 }
 class Json
 {
+	private $array=array();
 	//0:not begin
 	//1:in begin
 	//2:finish
-	private $array=array();
 	private $start=0;
 	private $input=null;
 	private $string="";
@@ -24,8 +40,10 @@ class Json
 	private $char_next_next_next='';
 	private $char_next_next_next_next='';
 	private $status=JsonStatus::Normal;
-	private $key="";
+	private $str="";
+	private $type=null;
 	private $value="";
+	private $value_type=null;
 	private function string_escape($input)
 	{
 		$input=str_ireplace(array("\\r","\\n","\\r","\\n"),"\n",$input);
@@ -37,6 +55,10 @@ class Json
 		$input=str_replace("\\'","'",$input);
 		$input=str_replace("\\\"","\"",$input);
 		return $input;
+	}
+	private function is_number($input)
+	{
+		return is_numeric($input);	
 	}
 	public function decode($input)
 	{
@@ -85,7 +107,8 @@ class Json
 					}
 					else if($this->char == '"')
 					{
-						$this->status=JsonStatus::Key;
+						$this->status=JsonStatus::String;
+						$this->type=JsonPosition::Key;
 					}
 					else if($this->char == ':')
 					{
@@ -94,110 +117,73 @@ class Json
 				}
 				else if($this->status == JsonStatus::Value)
 				{
-
+					if($this->char == ' ' || $this->char == '\t' || $this->char == '\n' || $this->char == '\r')
+					{
+						//skip
+					}
+					else if($this->char == ',')
+					{
+						$this->value='';
+						$this->value_type=null;
+						$this->status=JsonStatus::Normal;
+					}
+					else if($this->char == ']' || $this->char == '}')
+					{
+						$this->value='';
+						$this->value_type=null;
+						$this->status=JsonStatus::Normal;
+						$this->start=2;
+					}
+					//<number>
+					else if($this->value == '' && 
+						(
+							$this->is_number($this->char) ||
+							$this->char == '-' && $this->is_number($this->char_next) ||
+							$this->char == '.' && $this->is_number($this->char_next)
+						)
+					)
+					{
+						print "NUMBER>>>" . $this->char."\n";
+						$this->value_type=JsonType::Integer;
+					}
+					else if($this->value == "" && $this->char == '"')
+					{
+						$this->value_type=JsonType::String;
+						$this->status=JsonStatus::String;
+						$this->type=JsonPosition::Value;
+					}
+					else
+					{
+						print ">>>" . $this->char."\n";
+					}
 				}
-				else if($this->status == JsonStatus::Key)
+				else if($this->status == JsonStatus::String)
 				{
 					if($this->char == '"' && $this->char_prev !='\\')
 					{
 						//require : 
-						$this->key=$this->string_escape($this->key);
-						print $this->key."\n";
-						$this->key="";
-						$this->status=JsonStatus::Normal;
+						//print $this->str."\n";
+						$this->str=$this->string_escape($this->str);
+						if($this->type == JsonPosition::Key)
+						{
+							print "Key : " . $this->str."\n";
+							$this->key=$this->str;
+							$this->status=JsonStatus::Normal;
+						}
+						else if($this->type == JsonPosition::Value)
+						{
+							print "Value : " . $this->str."\n";
+							$this->value=$this->str;
+							$this->status=JsonStatus::Value;
+						}
+						$this->str="";
 					}
 					else
 					{
-						$this->key.=$this->char;
-						/*
-						if
-						(
-							//Windows CR+LF (\r\n)
-							//Linux LF (\n)
-							//OSX CR (\r)
-							$this->char == '\\' && $this->char_next != '' && 
-							(
-								$this->char_next == 'a' ||
-								$this->char_next == 'c' && $this->char_next_next == 'x' ||
-								$this->char_next == 'e' ||
-								$this->char_next == 'f' ||
-								$this->char_next == 'n' ||
-								//$this->char_next == 'p' && $this->char_next_next != '' && $this->char_next_next_next !='' ||
-								//$this->char_next == 'P' && $this->char_next_next != '' && $this->char_next_next_next !='' ||
-								$this->char_next == 'r' ||
-								$this->char_next == 'R' ||
-								$this->char_next == 't' ||
-								//$this->char_next == 'x' && $this->char_next_next != '' && $this->char_next_next_next !='' ||
-								//$this->char_next == 'd' && $this->char_next_next != '' && $this->char_next_next_next !='' ||
-								$this->char_next == '8' && $this->char_next_next == '1' ||
-								$this->char_next == '3' && $this->char_next_next == '7' && $this->char_next_next_next == '7' ||
-								$this->char_next == '1' && $this->char_next_next == '3' && $this->char_next_next_next == '3' ||
-								$this->char_next == '0' && $this->char_next_next == '1' && $this->char_next_next_next == '1' && $this->char_next_next_next_next == '3' ||
-								$this->char_next == '0' && $this->char_next_next == '1' && $this->char_next_next_next == '1' ||
-								$this->char_next == '1' && $this->char_next_next == '1' ||
-								$this->char_next == '7' ||
-								$this->char_next == '4' && $this->char_next_next == '0' ||
-								$this->char_next == '0' && $this->char_next_next == '4' && $this->char_next_next_next == '0' ||
-								$this->char_next == 'd' ||
-								$this->char_next == 'D' ||
-								$this->char_next == 'h' ||
-								$this->char_next == 'H' ||
-								$this->char_next == 's' ||
-								$this->char_next == 'S' ||
-								$this->char_next == 'v' ||
-								$this->char_next == 'V' ||
-								$this->char_next == 'w' ||
-								$this->char_next == 'W' ||
-								$this->char_next == 'b' ||
-								$this->char_next == 'B' ||
-								$this->char_next == 'A' ||
-								$this->char_next == 'Z' ||
-								$this->char_next == 'z' ||
-								$this->char_next == 'G' ||
-								$this->char_next == '"' ||
-								$this->char_next == '\''
-							)
-						)
-						{
-							if($this->char_next == 't')
-							{
-								$this->offset++;
-								$this->key.="\t";
-							}
-							else if($this->char_next == 'n')
-							{
-								$this->offset++;
-								$this->key.="\n";
-							}
-							else if($this->char_next == 'n')
-							{
-								$this->offset++;
-								$this->key.="\n";
-							}
-							else if($this->char_next == '"')
-							{
-								$this->offset++;
-								$this->key.="\"";
-							}
-							else if($this->char_next == '\'')
-							{
-								$this->offset++;
-								$this->key.="\"";
-							}
-							else
-							{
-								exit("Error!\nNot Support \...!");
-							}
-						}
-						else
-						{
-							$this->key.=$this->char;
-						}
-						*/
+						$this->str.=$this->char;
 					}
 				}
 			}
-			//print $this->char."\n";
 		}
 	}
 	public function encode($input)
