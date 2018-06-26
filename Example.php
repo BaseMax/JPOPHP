b844eb9ceb97697f9c9bc80f8381cef0eac33345
Max Base <MaxBaseCode@Gmail.Com>
2018-06-26 17:32:49 +0430

    Update Example.php

--- Example.php
+++ Example.php
@@ -1,7 +1,7 @@
 <?php
 include "JsonParser.php";
-/////////////////////////////////////////////
-$data=json_encode([1,2,3,4,5,"name"=>"ali"]);
+//////////////////////////////////////////////////////////
+$data=json_encode([1,2,3,4,5,"n\"am':=>[]{}e"=>"ali"]);
 print $data."\n";
 $array=$json->decode($data);
 print($array);
