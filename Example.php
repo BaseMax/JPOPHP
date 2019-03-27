869cf7cb04cbf2cc1d1aebd29e62b9f0d0836b99
Max Base <MaxBaseCode@Gmail.Com>
2019-03-27 14:27:58 +0430

    Update Example.php

--- Example.php
+++ Example.php
@@ -1,5 +1,66 @@
 <?php
 include "JsonParser.php";
+/* Version: 2.2 */
+print $json->encode([1,2,3,4])."\n";
+print $json->encode([1,2,[94,15,34,67],3,4,["name"=>"max"]])."\n";
+print $json->encode(["name"=>"max","age"=>49,"username"=>"BaseMax"])."\n";
+print $json->encode(["0"=>"max","1"=>49,"2"=>"BaseMax"])."\n";
+print $json->encode([0=>"max",1=>49,2=>"BaseMax"])."\n";
+print $json->encode([0=>"max","1.5"=>49,2=>"BaseMax"])."\n";
+print "\n\n";
+print $json->decode('[]')."\n";
+print $json->decode('{}')."\n";
+print $json->decode('["1"]')."\n";
+print $json->decode('["123456"]')."\n";
+print $json->decode('["hi"]')."\n";
+print $json->decode('["hi\""]')."\n";
+print $json->decode('["hi\"!"]')."\n";
+print $json->decode('[13]')."\n";
+print $json->decode('[134]')."\n";
+print $json->decode('[8]')."\n";
+print $json->decode('[0]')."\n";
+print $json->decode('[-9]')."\n";
+print $json->decode('[-945]')."\n";
+print $json->decode('[3.1]')."\n";
+print $json->decode('[3.145]')."\n";
+print_r($json->decode('			  [  -3.145,4,"test"]'));
+print_r($json->decode('			  [  -3.145,4,"test","name":"max"]'));
+print_r($json->decode('			  [  -3.145,4,"name","max",]'));
+print_r($json->decode('			  [  -3.145,4,"name","max",,,,,,,]'));
+print_r($json->decode('			  [,]'));
+print_r($json->decode('			  [,,,,]'));
+print_r($json->decode('			  [,,,,4]'));
+print_r($json->decode('{}'));
+print_r($json->decode('{"a":4,"6":945,,,}'));
+print_r($json->decode('{"a":4,"b":456,,,,}'));
+print_r($json->decode('{,"a":4,}'));
+print_r($json->decode('{,,,"a":4,"b":456,,,,}'));
+print_r($json->decode('{,,,,"a":4,,,,,,"6":945,,,}'));
+print_r($json->decode('[,,,,]'));
+print_r($json->decode('[4]'));
+print_r($json->decode(' [,,,,4]'));
+print_r($json->decode(' [  , , , , 4]'));
+print_r($json->decode(' [  , , , , 4 ]    '));
+print_r($json->decode(' [  , , 	,	 , 	4	 ]    '));
+print_r($json->decode("[\n]"));
+print_r($json->decode('["max",49,"BaseMax"]'));
+print_r($json->decode('[1,[]]'));
+print_r($json->decode('[1,[4]]'));
+print_r($json->decode('[1,[4,,,6,[8]]]'));
+print_r($json->decode('{"name":"Max","line":456,,,,}'));
+print_r($json->decode('{"name":"Max","line":456,,,,}'));
+// print_r($json->decode('{"name":"Max",4:555,,,,}')); // Error!
+print_r($json->decode('[null,false,true]'));
+$res=($json->decode('[null,false,true]'));
+var_dump($res[1]);
+var_dump($res);
+var_dump($json->decode('[null,false,true,]'));
+print_r($json->decode("[\"45\\\\\"]"));
+$arg='["4\\\\"]';
+var_dump($arg);
+print $arg."\n";
+print_r($json->decode($arg));
+
 /* Version: 0 - 2.0
 $data=json_encode([1,2,3,4,5,"n\"am':=>[]{}e"=>"ali"]);
 $data=json_encode(["name"=>"ali","int"=>110,"float"=>19.98,"-int"=>-110,"-float"=>-19.98,"bool"=>true]);
